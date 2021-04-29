da778526117e86b5a83ff68af0b1ac70720ec9c2
Yeradon <yeradon@gmail.com>
2021-04-29 10:02:29 +0200

    Added simple log of req

--- server.js
+++ server.js
@@ -21,6 +21,10 @@ var checkRateLimit = require('./lib/rate-limit')(process.env.CORSANYWHERE_RATELI
 
 var cors_proxy = require('./lib/cors-anywhere');
 cors_proxy.createServer({
+  handleInitialRequest: (req, res, dest) => {
+    console.log(req);
+    return false;
+  },
   originBlacklist: originBlacklist,
   originWhitelist: originWhitelist,
   requireHeader: ['origin', 'x-requested-with'],
